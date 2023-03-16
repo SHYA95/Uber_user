73fc6f578fe1ad9dc4936ddf6443cfbb698732b5
SHYA95 <yassershrouk254@gmail.com>
2023-03-16 13:18:44 +0200

    Initial Commit

---
+++ UberDriver2/UberDriver2App.swift
@@ -0,0 +1,17 @@
+//
+//  UberDriver2App.swift
+//  UberDriver2
+//
+//  Created by Shrouk Yasser on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberDriver2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
